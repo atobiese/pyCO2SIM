4e4cacbb81ddcf508eceb28d8907bf02c9f29aed
andrew hp_pc <andrew.tobiesen@sintef.no>
2020-08-04 12:42:27 +0200

    implmenents newtons method to the optimizer

--- client/test_RestAPI.py
+++ client/test_RestAPI.py
@@ -33,7 +33,7 @@ def get_summary_data(obj):
     in_vap_co2 = float(pobj['flow']) * pobj['molfrac'][ipy_co2]
     pobj = obj.get_pipe_struct(o.outputAbGObj)
     out_vap_co2 = float(pobj['flow']) * pobj['molfrac'][ipy_co2]
-    o.percentRemoved = (1 - out_vap_co2 / in_vap_co2) * 100
+    o.percentRemoved = (1.0 - out_vap_co2 / in_vap_co2) * 100
 
     o.reboilerDuty = obj.get_unit(o.reboilerObj, 'flashQ') / 3600  # kW
     reboilerTemp = obj.get_unit(o.reboilerObj, 'flashT') - 273.15
@@ -56,6 +56,55 @@ def get_summary_data(obj):
     print('simulation log can be found at port 5000  http://URL:5000/')
 
     return o
+
+def f(obj, duty):
+
+    # set new duty
+    obj.set_unit('Reboiler', 'flashQ', duty * 3600)
+    # solve flowsheet
+    obj.solve()
+    #get object function
+    o = get_summary_data(obj)
+    # objectbfunction
+    costfunc = o.percentRemoved
+
+    return costfunc
+
+def newtons_method(f, x0, epsilon, obj):
+    delta_x = 1.0
+    x_i = x0
+    f_i = f(obj, x_i)
+    x_i_1 = x_i + x0 * 0.001
+    Dx = x_i_1 - x_i
+    err_i = 1
+    stepsize = 10.0 #kW
+    setpsizecontrol = True
+    while (err_i) > epsilon:
+        f_i_1 = f(obj, x_i_1)
+        err_i = abs(f_i_1)
+        fdot = (f_i_1 - f_i) / (x_i_1 - x_i)
+
+        xNR = x_i_1 - f_i_1 / fdot
+
+        x_i = x_i_1
+        #x_i_1 = xNR
+
+        if setpsizecontrol is True:
+            next_deltastep = x_i_1 - xNR
+            controlled_deltastep = stepsize
+            if abs(next_deltastep) < controlled_deltastep:
+                x_i_1_ = xNR
+            else:
+                if (fdot < 0):
+                    x_i_1_ = x_i_1 + stepsize
+                else:
+                    x_i_1_ = x_i_1 - stepsize
+        else:
+            x_i_1_ = xNR
+
+        x_i_1 = x_i_1_
+        f_i = f_i_1
+
 
 def test(case):
     # case = "tcp_api"
@@ -80,31 +129,28 @@ def test(case):
         obj = ApiMatlab()
         obj.connect('ExampleTillerClosedLoop')
 
-    # solve flowsheet
-    obj.solve()
 
+
+    # solve flowsheet
+    # obj.set_unit('Reboiler', 'flashQ', 80 * 3600)
+    obj.solve()
 
     o = get_summary_data(obj)
+    o.percentRemoved
+    o.reboilerDuty
 
-    reboilerDuty_prev = obj.get_unit(o.reboilerObj, 'flashQ')
-    var = obj.set_unit('Reboiler', 'flashQ', reboilerDuty_prev*.95)
-    obj.solve()
-    get_summary_data(obj, o)
+    duty = o.reboilerDuty
+    cost = f(obj, duty)
 
-    reboilerDuty_prev = obj.get_unit(o.reboilerObj, 'flashQ')
-    var = obj.set_unit('Reboiler', 'flashQ', reboilerDuty_prev*.95)
-    obj.solve()
-    get_summary_data(obj, o)
+    # newtons_method(f, duty, 1e-3, obj)
 
-    reboilerDuty_prev = obj.get_unit(o.reboilerObj, 'flashQ')
-    var = obj.set_unit('Reboiler', 'flashQ', reboilerDuty_prev*.95)
-    obj.solve()
-    get_summary_data(obj, o)
+    duty = obj.get_unit(o.reboilerObj, 'flashQ')
+
 
 
 if __name__ == '__main__':
     # main file
     case = "tcp_api"
-    # case = "py2mat_api"
+    #case = "py2mat_api"
 
     test(case)
